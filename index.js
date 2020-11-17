dcb327578b549266f31c2f7533e43e42bf7b237e
Murtaza Sultani <murtaz1392@gmail.com>
2020-11-17 21:25:06 +0430

    Add rtl plugin

---
+++ index.js
@@ -0,0 +1,14 @@
+const selectorParser = require('postcss-selector-parser');
+
+module.exports = function({ addVariant, prefix }) {
+    addVariant('rtl', ({ modifySelectors, separator }) => {
+        modifySelectors(({ selector }) => {
+            return selectorParser((selectors) => {
+                selectors.walkClasses((sel) =>{
+                    sel.value = `rtl${separator}${sel.value}`
+                    sel.parent.insertBefore(sel, selectorParser().astSync(prefix('.direction-rtl ')))
+                })
+            }).processSync(selector)
+        })
+    })
+}
